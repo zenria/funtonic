520c14b2e8f7262b966c3efae8b4d5d64d0c61af
Philippe GASSMANN <philoops@gmail.com>
2020-01-28 09:28:54 +0100

    lto optimization

--- grpc-service/src/lib.rs
+++ grpc-service/src/lib.rs
@@ -1,3 +1,21 @@
+use thiserror::Error;
+
 pub mod grpc_protocol;
 
 pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+trait IntoGrpc {
+    type Output;
+
+    fn into_grpc(self) -> Self::Output;
+}
+
+trait FromGrpc<T> {
+    //fn try_from(shitty_grpc_struct: T) -> Result<Self, GrpcConstraintError>;
+}
+
+#[derive(Debug, Error)]
+enum GrpcConstraintError {
+    #[error("The field `{0}` is mandatory")]
+    MandatoryField(String),
+}
