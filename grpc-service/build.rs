d3c0159032807ca7d61b43407974de1fb4a44911
Philippe GASSMANN <philoops@gmail.com>
2022-08-18 15:22:26 +0200

    bundle protoc to please our CI

--- grpc-service/build.rs
+++ grpc-service/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    std::env::set_var("PROTOC", protobuf_src::protoc());
     tonic_build::configure()
         .out_dir("src/")
         .compile(&["proto/tasks/tasks.proto"], &["proto/tasks"])
