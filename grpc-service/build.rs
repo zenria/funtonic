c23063577ccbe6184c611237dc4f80bfcc79fe37
Philippe GASSMANN <philoops@gmail.com>
2025-08-14 11:54:53 +0200

    upgrade to latest tonic

--- grpc-service/build.rs
+++ grpc-service/build.rs
@@ -1,8 +1,8 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("protoc path: {}", protobuf_src::protoc().to_string_lossy());
     std::env::set_var("PROTOC", protobuf_src::protoc());
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .out_dir("src/")
-        .compile(&["proto/tasks/tasks.proto"], &["proto/tasks"])
-        .unwrap();
+        .compile_protos(&["proto/tasks/tasks.proto"], &["proto/tasks"])?;
+    Ok(())
 }
